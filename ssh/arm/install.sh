0a70cc51aafd300c112b9fa6a86aa0c66357689a
Giorgio Ghirardo <shinrisama@gmail.com>
2025-11-29 10:38:44 +0900

    [fix] ssh6

--- ssh/arm/install.sh
+++ ssh/arm/install.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "[ssh-setup] Resetting Termux mirrors and updating packages..."
 echo "updated"
-pkg update -y
+
 pkg upgrade -y --option Dpkg::Options::="--force-confnew"
 
 echo "[ssh-setup] Reinstalling essential libraries..."

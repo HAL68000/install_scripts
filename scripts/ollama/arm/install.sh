beaeda6c47dfe1cd860d5b0af7004be60f1ece3d
Giorgio Ghirardo <shinrisama@gmail.com>
2026-01-08 08:54:44 +0900

    [fix] ollama7

--- scripts/ollama/arm/install.sh
+++ scripts/ollama/arm/install.sh
@@ -30,6 +30,7 @@ pkg install -y curl git build-essential
 progress 30
 
 status "[ollama] installing ubuntu and ollama"
+export PROOT_NO_SECCOMP=1
 proot-distro install ubuntu && proot-distro exec ubuntu -- bash -lc "apt update && apt upgrade -y && curl -fsSL https://ollama.com/install.sh | sh" && mkdir -p "$PREFIX/var/lib/ollama"
 progress 80
 

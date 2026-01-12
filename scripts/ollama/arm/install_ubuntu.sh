f0bb7ff681ee8ceb21efbe6f3334a034e4bde5a6
Giorgio Ghirardo <shinrisama@gmail.com>
2026-01-13 07:46:46 +0900

    [add] ollama

--- scripts/ollama/arm/install_ubuntu.sh
+++ scripts/ollama/arm/install_ubuntu.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-status "[ollama] installing ubuntu and ollama"
-proot-distro install ubuntu && proot-distro exec ubuntu -- bash -lc "apt update && apt upgrade -y && curl -fsSL https://ollama.com/install.sh | sh" && mkdir -p "$PREFIX/var/lib/ollama"
+status "[ollama] installing ubuntu"
+sh "proot-distro install ubuntu"
 progress 80
-
-
+status "[ollama] installing ollama"
+proot-distro exec ubuntu -- bash -lc "apt update && apt upgrade -y && curl -fsSL https://ollama.com/install.sh | sh" && mkdir -p "$PREFIX/var/lib/ollama
+progress 90
 echo "[ollama-bootstrap] Installation completed!"
 status "[ollama] completed"
 progress 100
